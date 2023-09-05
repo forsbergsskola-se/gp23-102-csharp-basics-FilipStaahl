08b4de62b665baa34ff8573a26c3b96708ef5b34
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-09-05 16:06:23 +0200

    Started on P8 (NOT FINISHED)

--- P8Conversion/Program.cs
+++ P8Conversion/Program.cs
@@ -1,3 +1,13 @@
-﻿Console.WriteLine("What's your age?");
-string answer = Console.ReadLine();
-int age = Convert.(answer);
+﻿using System.Globalization;
+
+float number = 5.6f;
+Console.WriteLine("Give me a number.");
+var input= Console.ReadLine();
+Console.WriteLine(number);
+double fractionalNumber = 5;
+Console.WriteLine(fractionalNumber);
+fractionalNumber = Convert.ToDouble(input, CultureInfo.InvariantCulture);
+Console.WriteLine(fractionalNumber);
+
+
+
