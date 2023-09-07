c58c8924f19bd3ee84a95630c426e59be2ae8576
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-09-07 11:21:32 +0200

    New assignments completed
    2-6

--- P11AssignmentsOperators/Program.cs
+++ P11AssignmentsOperators/Program.cs
@@ -1,14 +1,15 @@
-﻿/* Implement a program that takes two integers as input and displays their division result as a float.
- 
- Console.WriteLine("Welcome to the KM/h to M/s Converter!");
+﻿// Implement a program that takes two integers as input and displays their division result as a float.
+
+/*Console.WriteLine("Welcome to the KM/h to M/s Converter!");
 Console.WriteLine("Please type in a amount of KM/H that you want to convert");
 string userInput = Console.ReadLine();
 float userKM = float.Parse(userInput);
 float x = userKM / 3600 * 1000;
-Console.WriteLine($" The answer to {userInput} KM/h in M/s is {x}");*/
+Console.WriteLine($" The answer to {userInput} KM/h in M/s is {x}"); */
 
-/* Implement a program that takes two integers as input and displays their division result as a float.
-Console.WriteLine("Hello and welcome to your calculator!");
+// Implement a program that takes two integers as input and displays their division result as a float.
+
+/*Console.WriteLine("Hello and welcome to your calculator!");
 Console.WriteLine("What numbers can I divide for you?");
 string userInputX = Console.ReadLine();
 int x = Convert.ToInt32(userInputX);
@@ -19,8 +20,9 @@ float answer = (float) x / y;
 Console.WriteLine($"{userInputX} divided by {userInputY} equals {answer}");*/
 
 
-/* Write a program that calculates the remainder of dividing two integers and displays the result.
-Console.WriteLine("Hello and welcome to your calculator!");
+// Write a program that calculates the remainder of dividing two integers and displays the result.
+
+/* Console.WriteLine("Hello and welcome to your calculator!");
 Console.WriteLine("What numbers can I divide for you?");
 string userInputX = Console.ReadLine();
 int x = Convert.ToInt32(userInputX);
@@ -28,4 +30,48 @@ Console.WriteLine("Please add your second number");
 string userInputY = Console.ReadLine();
 int y = Convert.ToInt32(userInputY);
 int answer = y % x;
-Console.WriteLine($"{userInputX} divided by {userInputY} equals {answer}"); *
+Console.WriteLine($"{userInputX} divided by {userInputY} equals {answer}"); */
+
+
+// Develop a program that calculates the area of a circle using a float input for the radius and displays the result. (probably requires google for the Maths)
+
+/* Console.WriteLine("Hello! Can you give me the radius of the circle?");
+string userImputR = Console.ReadLine();
+int x = Convert.ToInt32(userImputR);
+double answer = 3.142 * x * x;
+Console.WriteLine($"The area of the circle is {answer}"); */
+
+
+// Write a program that takes an integer input and displays its negation using the unary minus operator.* (requires Google
+
+
+/*Console.WriteLine("Hello! Please give me a number to switch for you!");
+string userInputN = Console.ReadLine();
+int x = Convert.ToInt32();
+int x = userInputN;
+int y = -x;*/
+
+//Write a program that takes an integer input and displays its negation using the unary minus operator.* (requires Google)
+
+
+/*Console.Write("Enter a number: ");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput); // Attempt to parse the user input
+int negatedNumber = -number; // Applying the unary minus operator
+Console.WriteLine($"The negation of {number} is {negatedNumber}");*/
+
+// Develop a program that takes two integers as input and calculates their product using the multiplication operator.
+
+/* Console.WriteLine("Hello what would you like to multiply?");
+Console.WriteLine("Please enter your first number");
+string numberX = Console.ReadLine();
+int x = Convert.ToInt32(numberX);
+Console.WriteLine("Perfect, now add another number.");
+string numberY = Console.ReadLine();
+int y = Convert.ToInt32(numberY);
+float answer = x * y;
+Console.WriteLine($"The answer for {numberX} * {numberY} = {answer}"); */
+
+
+
+
