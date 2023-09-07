59bcbc99c41aa1c6a7b3baf0469b0a06e78c1b2c
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-09-07 13:33:27 +0200

    Boolean script made

--- P12Boolean/Program.cs
+++ P12Boolean/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type in your age.");
+string userInput = Console.ReadLine();
+int age = Convert.ToInt32(userInput);
+bool isChild = age < 0 - 13;
+Console.WriteLine($"You are concidered a child: {isChild}");
+bool isTeenager = age >= 13 && age <= 19;
+Console.WriteLine($"You are condidered a teenager: {isTeenager}");
+bool isAdult = age > 19;
+Console.WriteLine($"You are concidered a adult: {isAdult}");
